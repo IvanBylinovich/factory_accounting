e06e26f28a7c706ac8cb96df8e7809f3249c3694
IvanBylinovich <ib6020607@gmail.com>
2021-07-13 13:02:23 +0300

    Initial commit

---
+++ src/main/java/by/factory_accounting/FactoryAccountingApplication.java
@@ -0,0 +1,13 @@
+package by.factory_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryAccountingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/factory_accounting/FactoryAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.factory_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryAccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
